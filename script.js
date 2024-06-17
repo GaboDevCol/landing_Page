926fb3f440f5c9d8d4e5a7cd9fc664e345bbb620
oliverHuibagson20 <153572436+oliverHuibagson20@users.noreply.github.com>
2024-06-17 14:22:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -23,14 +23,23 @@ $(document).ready(function () {
   });
 });
 
-let btn= document.getElementById("toggle-dark-mode");
+let btn = document.getElementById("toggle-dark-mode");
 
 btn.addEventListener('click', ()=>{
+  //cambios en el contenedor
  let container = document.getElementsByClassName('container');
 container.forEach(elemento => {
   elemento.style.boxShadow = "0 0 5px green inset, 0 0 10px green, 0 0 20px green";
   elemento.color = "lightgreen";
-}
+});
+
+  //Cambiosen la clase product
+  let product = document.querySelectorAll(.'product');
+  product.forEach(producto =>{
+    producto.style.boxShadow = "0 0 7px green inset, 0 0 15px green, 0 0 25px green";
+  });
+   //Cambios en el cuerpo
+  document.getElementById('body').style.background = "Black";
 })
 
 
